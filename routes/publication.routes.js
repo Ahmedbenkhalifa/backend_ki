574a40094517238c8d4a04fa0ac07cab28ec3b29
Ahmedbenkhalifa <ahmedbenkhalifa1997@gmail.com>
2022-10-21 01:55:25 +0100

    fix bugs

---
+++ routes/publication.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  addPublication,
+  getAllPublications,
+  getPublicationByUserId,
+  getAllMyPublication,
+  updateStatusById,
+  getAllPublishPost,
+  publishPostById,
+  deleteById,
+} = require("../controllers/publication.controller");
+const { isAdmin } = require("../middlewares/isAdmin");
+const { isAuth } = require("../middlewares/isAuth");
+const router = express.Router();
+
+router.post("/addPublication", isAuth, addPublication);
+router.get("/getAllPublications", isAuth, getAllPublications);
+router.get("/getPubById/:ClientID", isAuth, getPublicationByUserId);
+router.get("/getAllMyPub", isAuth, getAllMyPublication);
+router.delete("/deleteById/:PublicationID", isAuth, deleteById);
+router.get("/getPublishPost", getAllPublishPost);
+router.put("/updateStatus/:PublicationID", isAdmin, updateStatusById);
+router.put("/publishPost/:PublicationID", publishPostById);
+
+module.exports = router;
